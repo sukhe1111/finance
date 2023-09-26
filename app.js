03404e11e0afcf4012d3c1e56264349d84fdd90a
sukhe <suhbatsamdan0824@gmail.com>
2023-09-26 19:38:55 +0800

    Event listeneruudiig uusgej ogloo

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ var uiController = (function () {})();
 var financeController = (function () {})();
 
 // Holbogc
-var appController = (function (uiController, financeController) {})(
-  uiController,
-  financeController
-);
+var appController = (function (uiController, financeController) {
+  var ctrlAddItem = function () {
+    // 1. Oruulah ugugdliig delgetsees olj avah
+    console.log("Delgetsees ogogdol avah");
+    // 2. Olj avsan ugugdluudiig sanhuugiin controllert damjuulj tend hadgaln
+    // 3. Olj avasan ugugdluudee web deeree tohiroh hesegt gargana
+    // 4. Tusviig tootsooln
+    // 5. Etssiin uldegdel, tootsoog delgetsend gargan
+  };
+  document.querySelector(".add__btn").addEventListener("click", function () {
+    ctrlAddItem();
+  });
+
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13) {
+      ctrlAddItem();
+    }
+  });
+})(uiController, financeController);
