62e650a594fc656de73bd141385b4b9a8780ae9d
sukhe <suhbatsamdan0824@gmail.com>
2023-09-26 19:33:32 +0800

    123

---
+++ app.js
@@ -0,0 +1,11 @@
+// Delgets
+var uiController = (function () {})();
+
+// Sanhuu
+var financeController = (function () {})();
+
+// Holbogc
+var appController = (function (uiController, financeController) {})(
+  uiController,
+  financeController
+);
